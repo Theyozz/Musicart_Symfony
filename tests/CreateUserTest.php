373679ba8ec0e1fb076df68301da5644d93524ee
Théo VM <theomaurin875@gmail.com>
2024-09-04 00:55:16 +0200

    feat: add phpUnit

---
+++ tests/CreateUserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+//TODO
+class CreateUserTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
